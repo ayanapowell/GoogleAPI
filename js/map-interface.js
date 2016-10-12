1af1e407228c261510400a41c62edd0bb1445747
Aimen Khakwani and Ayana Powell <aimen+ayana@>
2016-10-12 13:22:23 -0700

    Add functioning api

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,15 +1,32 @@
 var Map = require('./../js/map.js').mapModule;
 
 $(document).ready(function() {
-  // var inputCity, inputState, location, lat, lng, myLatLng;
-  // var newMap = new Map();
-  //
-  // $("#city-form").submit(function(event) {
-  //   event.preventDefault();
-  //   inputCity = $('#city').val();
-  //   inputState = $('#state').val();
-  //   location = newMap.getLocation(inputCity, inputState);
-  //   geolocationSuccess(location);
-  // });
 
+  $("#city-form").submit(function(event) {
+    event.preventDefault();
+    var newMap = new Map();
+    var inputCity = $('#city').val();
+    var inputState = $('#state').val();
+    var location = newMap.getLocation(inputCity, inputState);
+    setTimeout(function() {
+      initMap(location);
+    }, 1000);
+  });
 });
+
+function initMap(location) {
+
+   var myLatLng = {lat: location[0], lng: location[1]};
+   // Create a map object and specify the DOM element for display.
+   var map = new google.maps.Map(document.getElementById('map'), {
+     center: myLatLng,
+     scrollwheel: false,
+     zoom: 12
+   });
+   // Create a marker and set its position.
+   var marker = new google.maps.Marker({
+     map: map,
+     position: myLatLng,
+     title: 'Hello World!'
+   });
+   }
--- js/map.js
+++ js/map.js
@@ -1,31 +1,16 @@
 var mapApiKey = require('./../.env').mapApiKey;
-var latLng = [];
 function Map() {
-
 }
 
 Map.prototype.getLocation = function(city, state) {
+  var array = [];
   $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + ',+' + state + '&key=' + mapApiKey).then(function(response) {
     lat = response.results[0].geometry.location.lat;
     lng = response.results[0].geometry.location.lng;
-    latLng.push(lat, lng);
-    return latLng;
-    // console.log("Lat : " + lat);
-    // console.log("Lng : " + lng);
+    array.push(lat, lng);
   });
+  return array;
 }
-
-// Map.prototype.initialize = function(location) {
-//   lat = location[0];
-//   lng = location[1];
-//   myLatLng = new google.maps.LatLng(lng, lat);
-//   var myOptions = {
-//     zoom: 8,
-//     center: myLatlng,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   }
-//   var map = new google.maps.Map(document.getElementById("map"), myOptions);
-// };
 
 exports.mapModule = Map;
 
