92757ca0630b8447052997e614449db81b540807
aimen khakwani and ayana powell <aimen+ayana@>
2016-10-13 16:26:37 -0700

    add default book

--- js/book-interface.js
+++ js/book-interface.js
@@ -1,9 +1,12 @@
+
 var Book = require('./../js/book.js').bookModule;
 
 $(document).ready(function() {
   var newBook = new Book();
+  newBook.getDefault();
   $('#books-form').submit(function(event) {
     event.preventDefault();
+    $('.default_books').addClass('hide');
     var search = $('#search').val();
     newBook.searchBooks(search);
   });
--- js/book.js
+++ js/book.js
@@ -2,6 +2,24 @@ var bookApiKey = require('./../.env').bookApiKey;
 function Book() {
 }
 
+Book.prototype.getDefault = function() {
+  var books = [];
+  var that = this;
+  $.get('https://www.googleapis.com/books/v1/volumes?q=' + 'web'+ '&key=' + bookApiKey).then(function(response) {
+    var items = response.items;
+    for (var i = 0; i < 5; i++) {
+      var title = items[i].volumeInfo.title;
+      var authors = items[i].volumeInfo.authors[0];
+      var description = items[i].volumeInfo.description;
+      var rating = items[i].volumeInfo.averageRating;
+      var image = items[i].volumeInfo.imageLinks.thumbnail;
+      books.push([title, authors, description, rating, image]);
+    };
+    that.listDefaults(books);
+  });
+}
+
+
 Book.prototype.searchBooks = function(search) {
   var books = [];
   var that = this;
@@ -15,7 +33,6 @@ Book.prototype.searchBooks = function(search) {
       var image = item.volumeInfo.imageLinks.thumbnail;
       books.push([title, authors, description, rating, image]);
     });
-    console.log(books);
     that.listBooks(books);
   });
 }
@@ -24,6 +41,14 @@ Book.prototype.listBooks = function(books) {
   $('.all_books').empty();
   for (var i = 0; i < books.length; i++) {
     $('.all_books').append("<br><div class='row'><div class='col-sm-3'>" + '<img src="' + books[i][4] + '">' + "</div>" + "<div class='col-sm-8 col-sm-offset-1'><h3>" + books[i][0] + "</h3><br>" + "<p class='bold'>Author(s): " + books[i][1] + "</p><p class='bold'>Description: " + books[i][2] + "</p><p class='bold'>Average Rating: "
+    + books[i][3] + "</p></div></div><br>");
+  }
+}
+
+Book.prototype.listDefaults = function(books) {
+  $('.all_books').empty();
+  for (var i = 0; i < books.length; i++) {
+    $('.default_books').append("<br><div class='row'><div class='col-sm-3'>" + '<img src="' + books[i][4] + '">' + "</div>" + "<div class='col-sm-8 col-sm-offset-1'><h3>" + books[i][0] + "</h3><br>" + "<p class='bold'>Author(s): " + books[i][1] + "</p><p class='bold'>Description: " + books[i][2] + "</p><p class='bold'>Average Rating: "
     + books[i][3] + "</p></div></div><br>");
   }
 }
